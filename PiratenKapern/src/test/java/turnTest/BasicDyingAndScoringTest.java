f045cddae2e96054f09cf6de8839b1f47e5bf4f6
Dishy <dishysun@hotmail.com>
2020-10-22 15:19:55 -0400

    Cucumber test: 41-85

--- PiratenKapern/src/test/java/turnTest/BasicDyingAndScoringTest.java
+++ PiratenKapern/src/test/java/turnTest/BasicDyingAndScoringTest.java
@@ -3,19 +3,26 @@ package turnTest;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import cucumber.annotation.en.*;
 import entity.Dice;
+import game.ChestException;
 import game.Turn;
 
 public class BasicDyingAndScoringTest {
 	Turn turn;
 	ArrayList<Dice> dice;
 	
+	private void add(String diceStr, int number) {
+		for (int i = 0; i < number; i++) {
+			dice.add(new Dice(diceStr));
+		}
+	}
+	
 	@Given("a turn")
 	public void newTurn() {
 		turn = new Turn();
-		turn.setCard(new entity.FortuneCard.Gold());
 		dice = new ArrayList<Dice>();
 	}
 	
@@ -101,18 +108,18 @@ public class BasicDyingAndScoringTest {
 		turn.hold(Dice.Face.DIAMOND);
 	}
 	
+	//actions
+	@When("end turn")
+	public void endTurn() {
+		turn.endTurn();
+	}
+	
 	//38
 	@When("roll 3 skulls on firstroll")
 	public void roll3SkullsOnFirstRoll() {
-		
-		dice.add(new Dice("sk"));
-		dice.add(new Dice("sk"));
-		dice.add(new Dice("sk"));
-		dice.add(new Dice("mo"));
-		dice.add(new Dice("mo"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("mo"));
+		add("sk", 3);
+		add("mo", 3);
+		add("pa", 2);
 	}
 	
 	@Then("disqulified after firstroll")
@@ -124,21 +131,15 @@ public class BasicDyingAndScoringTest {
 	@When("first roll: 1 skull, 4 parrots, 3 swords")
 	public void firstRoll_39() {
 		dice.add(new Dice("sk"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("sw"));
-		dice.add(new Dice("sw"));
-		dice.add(new Dice("sw"));
+		add("pa", 4);
+		add("sw",3);
 		turn.firstRoll(dice);
 	}
 	
 	@When("reroll: 2 skull, 1 sword")
 	public void reroll_39_1() {
 		dice.clear();
-		dice.add(new Dice("sk"));
-		dice.add(new Dice("sk"));
+		add("sk",2);
 		dice.add(new Dice("sw"));
 	}
 	
@@ -150,16 +151,10 @@ public class BasicDyingAndScoringTest {
 	//40
 	@When("first roll: 2 skulls, 4 parrots, 2 swords")
 	public void firstRoll_40() {
-		dice.add(new Dice("sk"));
-		dice.add(new Dice("sk"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("pa"));
-		dice.add(new Dice("sw"));
-		dice.add(new Dice("sw"));
-		turn.firstRoll(dice);
+		add("sk", 2);
+		add("pa", 4);
+		add("sw", 2);
+		assertFalse(turn.firstRoll(dice));
 	}
 	
 	@When("reroll: 1 skull, 1 sword")
@@ -170,4 +165,548 @@ public class BasicDyingAndScoringTest {
 	}
 	
 	//41
+	@When("reroll: 1 skull, 2 monkey")
+	public void reroll_41_1() {
+		dice.clear();
+		dice.add(new Dice("sk"));
+		dice.add(new Dice("mo"));
+		dice.add(new Dice("mo"));
+		assertEquals(0,turn.reroll(dice));
+	}
+	
+	@When("lock all and reroll monkey")
+	public void lockAllRerollMonkey() {
+		turn.lockAll();
+		turn.unhold(Dice.Face.MONKEY);
+	}
+	
+	@When("reroll: 1 skull, 1 monkey")
+	public void reroll_41_2() {
+		dice.clear();
+		dice.add(new Dice("sk"));
+		dice.add(new Dice("mo"));
+	}
+	
+	//43
+	@When("first roll: 2 diamond, 2 coin, 4 others with no score")
+	public void firstRoll_42() {
+		dice.add(new Dice("di"));
+		dice.add(new Dice("di"));
+		dice.add(new Dice("co"));
+		dice.add(new Dice("co"));
+		dice.add(new Dice("pa"));
+		dice.add(new Dice("pa"));
+		dice.add(new Dice("mo"));
+		dice.add(new Dice("mo"));
+		assertFalse(turn.firstRoll(dice));
+		System.err.println(turn.statString());
+	}
+	
+	@Then("score: 800")
+	public void score800() {
+		assertEquals(800, turn.getDelta().getChange());
+	}
+	
+	//44
+	@When("first roll: 2 monkey, 6 others no score")
+	public void firstRoll_44() {
+		dice.add(new Dice("mo"));
+		dice.add(new Dice("mo"));
+		dice.add(new Dice("sk"));
+		dice.add(new Dice("sk"));
+		dice.add(new Dice("pa"));
+		dice.add(new Dice("pa"));
+		dice.add(new Dice("sw"));
+		dice.add(new Dice("sw"));
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@When("reroll: 1 monkey, 2 parrot, 1 sword")
+	public void reroll_44_1(){
+		dice.clear();
+		dice.add(new Dice("mo"));
+		dice.add(new Dice("pa"));
+		dice.add(new Dice("pa"));
+		dice.add(new Dice("sw"));
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@Then("score: 200")
+	public void score200() {
+		assertEquals(200, turn.getDelta().getChange());
+	}
+	
+	//45
+	@When("first roll: 3 monkey, 3 swords, 2 skull")
+	public void firstRoll_45() {
+		add("mo", 3);
+		add("sw", 3);
+		add("sk", 2);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@Then("score: 300")
+	public void score300() {
+		assertEquals(300, turn.getDelta().getChange());
+	}
+	
+	//46
+	@When("first roll: 2 monkey, 2 parrot, 2 sword, 2 skull")
+	public void firstRoll_46() {
+		dice.add(new Dice("mo"));
+		dice.add(new Dice("mo"));
+		dice.add(new Dice("pa"));
+		dice.add(new Dice("pa"));
+		dice.add(new Dice("sw"));
+		dice.add(new Dice("sw"));
+		dice.add(new Dice("sk"));
+		dice.add(new Dice("sk"));
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@When("reroll: 1 monkey, 1 parrot")
+	public void reroll_46_1() {
+		dice.clear();
+		dice.add(new Dice("mo"));
+		dice.add(new Dice("pa"));
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	//47
+	@When("first roll: 3 diamonds, 2 skull, 1 sword, 2 parrot")
+	public void firstRoll_47() {
+		add("di", 3);
+		add("sk", 2);
+		add("sw", 1);
+		add("pa", 2);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@Then("score: 500")
+	public void score500() {
+		assertEquals(500, turn.getDelta().getChange());
+	}
+	
+	//48
+	@When("first roll: 4 coin, 2 skull, 2 parrot")
+	public void firstRoll_48() {
+		add("co", 4);
+		add("sk", 2);
+		add("pa", 2);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@Then("score: 700")
+	public void score700() {
+		assertEquals(700, turn.getDelta().getChange());
+	}
+	
+	//49
+	@When("first roll: 3 sword, 4 parrot, 1 monkey")
+	public void firstRoll_49() {
+		add("sw", 3);
+		add("pa", 4);
+		add("mo", 1);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@Then("score: 400")
+	public void score400() {
+		assertEquals(400, turn.getDelta().getChange());
+	}
+	
+	//50
+	@When("first roll: 2 coin, 2 sword, 2 parrot, 2 monkey")
+	public void firstRoll_50() {
+		add("co", 2);
+		add("sw", 2);
+		add("pa", 2);
+		add("mo", 2);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@When("reroll: 1 coin, 1 monkey, 2 parrot")
+	public void reroll_50_1() {
+		dice.clear();
+		add("co", 1);
+		add("mo", 1);
+		add("pa", 2);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@When("reroll: 1 sword, 2 monkey")
+	public void reroll_50_2() {
+		dice.clear();
+		add("sw", 1);
+		add("mo", 2);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@When("reroll: 1 sword, 1 skull")
+	public void reroll_50_3() {
+		dice.clear();
+		add("sw", 1);
+		add("sk", 1);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	//52
+	@When("first roll: 2 sword, 6 monkey")
+	public void firstRoll_52() {
+		add("sw", 2);
+		add("mo", 6);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@When("reroll: 1 sword, 5 monkey")
+	public void reroll_52_1() {
+		dice.clear();
+		add("sw", 1);
+		add("mo", 5);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@When("reroll: 1 sword, 4 monkey")
+	public void reroll_52_2() {
+		dice.clear();
+		add("sw", 1);
+		add("mo", 4);
+		assertEquals(0, turn.reroll(dice));
+	}
+
+	@When("reroll: 1 sword, 2 monkey, 1 parrot")
+	public void reroll_52_3() {
+		dice.clear();
+		add("sw", 1);
+		add("mo", 2);
+		add("pa", 1);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@Then("score: 600")
+	public void score600() {
+		assertEquals(600, turn.getDelta().getChange());
+	}
+	
+	//53
+	@Then("score: 1100")
+	public void score1100() {
+		assertEquals(1100,turn.getDelta().getChange());
+	}
+	
+	//54
+	@When("first roll: 7 parrot, 1 skull")
+	public void firstRoll_54() {
+		add("pa", 7);
+		add("sk" ,1);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@Then("score: 2100")
+	public void score2100() {
+		assertEquals(2100, turn.getDelta().getChange());
+	}
+	
+	//55
+	@When("first roll: 8 coin")
+	public void firstRoll_55() {
+		add("co", 8);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@Then("score: 5400")
+	public void score5400() {
+		assertEquals(5400, turn.getDelta().getChange());
+	}
+	
+	//57
+	@When("first roll: 8 sword")
+	public void firstRoll_57() {
+		add("sw", 8);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@Then("score: 9000")
+	public void score9000() {
+		assertEquals(9000, turn.getDelta().getChange());
+	}
+	
+	//58
+	@When("first roll: 4 monkey, 4 sword")
+	public void firstRoll_58() {
+		add("mo", 4);
+		add("sw", 4);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@When("reroll: 2 monkey, 2 parrot")
+	public void reroll_58_1() {
+		dice.clear();
+		add("mo", 2);
+		add("pa", 2);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@When("reroll: 2 monkey")
+	public void reroll_58_2() {
+		dice.clear();
+		add("mo", 2);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@Then("score: 4600")
+	public void score4600() {
+		assertEquals(4600, turn.getDelta().getChange());
+	}
+	
+	//59
+	@When("reroll: 2 diamond, 2 parrot, 2 skull, 2 monkey")
+	public void reroll_59() {
+		dice.clear();
+		add("di", 2);
+		add("pa", 2);
+		add("sk", 2);
+		add("mo", 2);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	//60
+	@When("reroll: 3 diamond, 2 skull, 1 parrot, 2 monkey")
+	public void reroll_60() {
+		dice.clear();
+		add("di", 3);
+		add("sk", 2);
+		add("pa", 1);
+		add("mo", 2);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	//61
+	@When("reroll: 3 coin, 2 skull, 1 parrot, 2 monkey")
+	public void reroll_61() {
+		dice.clear();
+		add("co", 3);
+		add("sk", 2);
+		add("pa", 1);
+		add("mo", 2);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	//63
+	@When("first roll: 4 monkey, 3 coin, 1 skull")
+	public void firstRoll_63() {
+		add("mo", 4);
+		add("co", 3);
+		add("sk", 1);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	//65
+	@When("And first roll: 7 sword, 1 monkey")
+	public void firstRoll_65() {
+		add("sw", 7);
+		add("mo", 1);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@Then("reroll fail: roll at least 2 dice")
+	public void reroll_65() {
+		assertEquals(-1, turn.reroll(null));
+	}
+	
+	//70
+	@When("first roll: 2 skull, 6 coin")
+	public void firstRoll_70() {
+		add("sk", 2);
+		add("co", 6);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@When("unlock one of skulls")
+	public void unlock_70() {
+		HashSet<Integer> set = new HashSet<Integer>();
+		set.add(0);
+		turn.unlock(set);
+	}
+	
+	@When("reroll: 7 sward")
+	public void reroll_70() {
+		dice.clear();
+		add("sw", 7);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@Then("only 1 skull in hand")
+	public void then_70() {
+		int skull = 0;
+		for(Dice d: turn.getHand()) {
+			if (d.getFace() == Dice.Face.SKULL) skull++;
+		}
+		assertEquals(1, skull);
+	}
+	
+	//71
+	@When("reroll: 1 skull,1 coin")
+	public void reroll_71_1() {
+		dice.clear();
+		add("sk", 1);
+		add("co", 1);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@When("reroll: 2 parrot")
+	public void reroll_71_2() {
+		dice.clear();
+		add("pa", 2);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@Then("score: 1000")
+	public void score1000() {
+		assertEquals(1000, turn.getDelta().getChange());
+	}
+	
+	//72
+	@Then("no skull in hand")
+	public void then_72() {
+		int skull = 0;
+		for (Dice d : turn.getHand()) {
+			if (d.getFace() == Dice.Face.SKULL) skull++;
+		}
+		assertEquals(0, skull);
+	}
+	
+	//75
+	@When("first roll: 3 monkey, 3 parrot,  1 skull, 1 coin")
+	public void firstRoll_75() {
+		add("mo", 3);
+		add("pa", 3);
+		add("sk", 1);
+		add("co", 1);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	//76
+	@When("reroll: 2 monkey, 1 parrot, 2 coin, 1 diamond, 2 sword")
+	public void reroll_76() {
+		dice.clear();
+		add("mo", 2);
+		add("pa", 1);
+		add("co", 2);
+		add("di", 1);
+		add("sw", 2);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@When("reroll: 3 monkey, 4 parrot, 1 sword")
+	public void reroll_77() {
+		dice.clear();
+		add("mo", 3);
+		add("pa", 4);
+		add("sw", 1);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	@Then("score: 2000")
+	public void score2000() {
+		assertEquals(2000, turn.getDelta().getChange());
+	}
+	
+	//80
+	@When("first roll: 3 parrot, 2 sword, 2 diamond, 1 coin")
+	public void firstRoll_80() {
+		add("pa", 3);
+		add("sw", 2);
+		add("di", 2);
+		add("co", 1);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@When("stash: index 5 6 7")
+	public void stash_80_1() {
+		HashSet<Integer> set = new HashSet<Integer>();
+		set.add(5);
+		set.add(6);
+		set.add(7);
+		try {
+			turn.moveToChest(set);
+		} catch (ChestException e) {
+			fail("Should have treasure chest");
+		}
+	}
+	
+	@When("stash: index 0 1 2 3 4")
+	public void stash_80_2() {
+		HashSet<Integer> set = new HashSet<Integer>();
+		set.add(0);
+		set.add(1);
+		set.add(2);
+		set.add(3);
+		set.add(4);
+		try {
+			turn.moveToChest(set);
+		} catch (ChestException e) {
+			fail("Should have treasure chest");
+		}
+	}
+	
+	@When("withdraw: index 0 1 2")
+	public void withdraw_80() {
+		HashSet<Integer> set = new HashSet<Integer>();
+		set.add(0);
+		set.add(1);
+		set.add(2);
+		try {
+			turn.moveToHand(set);
+		} catch (ChestException e) {
+			fail("Should have treasure chest");
+		}
+	}
+	
+	@When("reroll: 1 skull, 1 coin, 1 parrot")
+	public void reroll_80_2() {
+		dice.clear();
+		add("sk", 1);
+		add("co", 1);
+		add("pa", 1);
+		assertEquals(0, turn.reroll(dice));
+	}
+	
+	//85
+	@When("first roll:  2 skull, 3 parrot, 3 coin")
+	public void firstRoll_85() {
+		add("sk",2);
+		add("pa",3);
+		add("co",3);
+		assertFalse(turn.firstRoll(dice));
+	}
+	
+	@When("reroll: 2 diamond, 1 coin")
+	public void reroll_85_1() {
+		dice.clear();
+		add("di", 2);
+		add("co", 1);
+		assertEquals(0,turn.reroll(dice));
+	}
+	
+	@When("stash: index 4")
+	public void stash_85_2() {
+		HashSet<Integer> set = new HashSet<Integer>();
+		set.add(4);
+		try {
+			turn.moveToChest(set);
+		} catch (ChestException e) {
+			fail("Should have treasure chest");
+		}
+	}
+	
+	@When("reroll: 1 skull, 1 coin")
+	public void reroll_85_2() {
+		dice.clear();
+		add("sk", 1);
+		add("co", 1);
+		assertEquals(1,turn.reroll(dice));
+	}
+	
+	
 }
--- PiratenKapern/src/test/resources/turnTest/basic_dying.feature
+++ PiratenKapern/src/test/resources/turnTest/basic_dying.feature
@@ -27,4 +27,311 @@ Feature: basic dying test
 		Then disqulified: 3 or more skulls
 		
 	@tag41
-	Scenario: die with 1 skulls on second roll
+	Scenario: die with 1 skulls on second reroll
+		Given a turn
+		When fortune card is coin
+		And first roll: 1 skull, 4 parrots, 3 swords
+		And hold parrot
+		And reroll: 1 skull, 2 monkey
+		And lock all and reroll monkey
+		And reroll: 1 skull, 1 monkey
+		Then disqulified: 3 or more skulls
+		
+	@tag43
+	Scenario: firstroll score 800 with captain, 2 diamon, 2 coin,
+		Given a turn
+		When fortune card is captain
+		And first roll: 2 diamond, 2 coin, 4 others with no score
+		And end turn
+		Then score: 800
+		
+	@tag44
+	Scenario: coin, firstroll: 2 monkey, reroll: 1 monkey
+		Given a turn
+		When fortune card is coin
+		When first roll: 2 monkey, 6 others no score
+		And hold monkey
+		And reroll: 1 monkey, 2 parrot, 1 sword
+		And end turn
+		Then score: 200
+		
+	@tag45
+	Scenario: coin, score 2 sets of 3 monkey, swords in RTS on first roll
+		Given a turn
+		When fortune card is coin
+		And first roll: 3 monkey, 3 swords, 2 skull
+		And end turn
+		Then score: 300
+		
+	@tag46
+	Scenario: score 2 sets of 3 monkey, parrots in RTS using 2 rolls
+		Given a turn
+		When fortune card is coin
+		And first roll: 2 monkey, 2 parrot, 2 sword, 2 skull
+		And hold monkey
+		And hold parrot
+		And reroll: 1 monkey, 1 parrot
+		And end turn
+		Then score: 300
+		
+	@tag47
+	Scenario: score a set of 3 diamonds correctly
+		Given a turn
+		When fortune card is coin
+		And first roll: 3 diamonds, 2 skull, 1 sword, 2 parrot
+		And end turn
+		Then score: 500
+	
+	@tag48
+	Scenario: score a set of 4 coins correctly with FC is a diamond
+		Given a turn
+		When fortune card is diamond
+		And first roll: 4 coin, 2 skull, 2 parrot
+		And end turn
+		Then score: 700
+		
+	@tag49
+	Scenario: score set of 3 swords and set of 4 parrots correctly on first roll
+		Given a turn
+		When fortune card is coin
+		And first roll: 3 sword, 4 parrot, 1 monkey
+		And end turn
+		Then score: 400
+		
+	@tag50
+	Scenario: score set of 3 coins+ FC and set of 4 swords correctly over several rolls
+		Given a turn
+		When fortune card is coin
+		And first roll: 2 coin, 2 sword, 2 parrot, 2 monkey
+		And hold coin
+		And hold sword
+		And reroll: 1 coin, 1 monkey, 2 parrot
+		And hold coin
+		And hold sword
+		And reroll: 1 sword, 2 monkey
+		And hold coin
+		And hold sword
+		And reroll: 1 sword, 1 skull
+		And end turn
+		Then score: 800
+		
+	@tag51
+	Scenario: same as previous row but with captain fortune card
+	Given a turn
+		When fortune card is captain
+		And first roll: 2 coin, 2 sword, 2 parrot, 2 monkey
+		And hold coin
+		And hold sword
+		And reroll: 1 coin, 1 monkey, 2 parrot
+		And hold coin
+		And hold sword
+		And reroll: 1 sword, 2 monkey
+		And hold coin
+		And hold sword
+		And reroll: 1 sword, 1 skull
+		And end turn
+		Then score: 1200
+		
+	@tag52
+	Scenario: score set of 5 swords over 3 rolls
+		Given a turn
+		When fortune card is coin
+		And first roll: 2 sword, 6 monkey
+		And hold sword
+		And reroll: 1 sword, 5 monkey
+		And hold sword
+		And reroll: 1 sword, 4 monkey
+		And hold sword
+		And reroll: 1 sword, 2 monkey, 1 parrot
+		And end turn;
+		Then score: 600
+	
+	@tag53
+	Scenario: score set of 6 monkeys on first roll
+		Given a turn
+		When fortune card is coin
+		And first roll: 2 sword, 6 monkey
+		And end turn
+		Then score: 1100
+		
+	@tag54
+	Scenario: score set of 7 parrots on first roll
+		Given a turn
+		When fortune card is coin
+		And first roll: 7 parrot, 1 skull
+		And end turn
+		Then score: 2100
+		
+	@tag55
+	Scenario: score set of 8 coins on first roll
+		Given a turn
+		When fortune card is coin
+		And first roll: 8 coin
+		And end turn
+		Then score: 5400
+		
+	@tag56
+	Scenario: score set of 8 coins on first roll and FC is diamond 
+		Given a turn
+		When fortune card is diamond
+		And first roll: 8 coin
+		And end turn
+		Then score: 5400
+		
+	@tag57
+	Scenario: score set of 8 swords on first roll and FC is captain
+		Given a turn
+		When fortune card is captain
+		And first roll: 8 sword
+		And end turn
+		Then score: 9000
+		
+	@tag58
+	Scenario: score set of 8 monkeys over several rolls
+		Given a turn
+		When fortune card is coin
+		And first roll: 4 monkey, 4 sword
+		And hold monkey
+		And reroll: 2 monkey, 2 parrot
+		And hold monkey
+		And reroll: 2 monkey
+		And end turn
+		Then score: 4600
+		
+	@tag59
+	Scenario: score a set of 2 diamonds over 2 rolls with FC is diamond
+		Given a turn
+		When fortune card is diamond
+		And first roll: 4 monkey, 4 sword
+		And reroll: 2 diamond, 2 parrot, 2 skull, 2 monkey
+		And end turn
+		Then score: 400
+		
+	@tag60
+	Scenario: score a set of 3 diamonds over 2 rolls
+		Given a turn
+		When fortune card is coin
+		And first roll: 4 monkey, 4 sword
+		And reroll: 3 diamond, 2 skull, 1 parrot, 2 monkey
+		And end turn
+		Then score: 500
+		
+	@tag61
+	Scenario: score a set of 3 coins over 2 rolls
+		Given a turn
+		When fortune card is coin
+		And first roll: 4 monkey, 4 sword
+		And reroll: 3 coin, 2 skull, 1 parrot, 2 monkey
+		And end turn
+		Then score: 600
+		
+	@tag62
+	Scenario: score a set of 3 coins over 2 rolls  with FC is diamond
+		Given a turn
+		When fortune card is diamond
+		And first roll: 4 monkey, 4 sword
+		And reroll: 3 coin, 2 skull, 1 parrot, 2 monkey
+		And end turn
+		Then score: 500
+		
+	@tag63
+	Scenario: score a set of 4 monkeys and a set of 3 coins
+		Given a turn
+		When fortune card is coin
+		And first roll: 4 monkey, 3 coin, 1 skull
+		And end turn
+		Then score: 800
+		
+	@tag65
+	Scenario: get 7 swords on first roll, try to roll the 8 die by itself
+		Given a turn
+		When fortune card is coin
+		And first roll: 7 sword, 1 monkey
+		And hold sword
+		Then reroll fail: roll at least 2 dice
+		
+	@tag70
+	Scenario: roll 2 skulls, reroll one of them due to sorceress, then go to next round of turn
+		Given a turn
+		When fortune card is sorceress
+		And first roll: 2 skull, 6 coin
+		And unlock one of skulls
+		And reroll: 7 sward
+		Then only 1 skull in hand
+		
+	@tag71
+	Scenario: roll no skulls, then next round roll 1 skull and reroll for it, then score 
+		Given a turn
+		When fortune card is sorceress
+		And first roll: 2 sword, 6 monkey
+		And hold monkey
+		And reroll: 1 skull,1 coin
+		And unlock one of skulls
+		And reroll: 2 parrot
+		And end turn
+		Then score: 1000
+		
+	@tag72
+	Scenario: roll no skulls, then next round roll 1 skull and reroll for it, then go to next round 
+		Given a turn
+		When fortune card is sorceress
+		And first roll: 2 sword, 6 monkey
+		And hold monkey
+		And reroll: 1 skull,1 coin
+		And unlock one of skulls
+		And reroll: 2 parrot
+		Then no skull in hand
+		
+	@tag75
+	Scenario: first roll gets 3 monkeys 3 parrots  1 skull 1 coin
+		Given a turn
+		When fortune card is monkey business
+		And first roll: 3 monkey, 3 parrot,  1 skull, 1 coin
+		And end turn
+		Then score: 1100
+		
+	@tag76
+	Scenario: over several rolls: 2 monkeys, 1 parrot, 2 coins, 1 diamond, 2 swords
+		Given a turn
+		When fortune card is monkey business
+		And first roll: 4 monkey, 4 sword
+		And reroll: 2 monkey, 1 parrot, 2 coin, 1 diamond, 2 sword
+		And end turn
+		Then score: 400
+	
+	@tag77
+	Scenario: over several rolls get 3 monkeys, 4 parrots, 1 sword
+		Given a turn
+		When fortune card is monkey business
+		And first roll: 4 monkey, 4 sword
+		And reroll: 2 monkey, 1 parrot, 2 coin, 1 diamond, 2 sword
+		And reroll: 3 monkey, 4 parrot, 1 sword
+		And end turn
+		Then score: 2000
+		
+	@tag80
+	Scenario: treasure chest scenario 1
+		Given a turn
+		When fortune card is treasure chest
+		And first roll: 3 parrot, 2 sword, 2 diamond, 1 coin
+		And stash: index 5 6 7
+		And hold parrot
+		And reroll: 2 parrot
+		And stash: index 0 1 2 3 4
+		And withdraw: index 0 1 2
+		And reroll: 1 skull, 1 coin, 1 parrot
+		And end turn
+		Then score: 1100
+		
+	@tag85
+	Scenario: treasure chest scenario 2
+		Given a turn
+		When fortune card is treasure chest
+		And first roll:  2 skull, 3 parrot, 3 coin
+		And stash: index 5 6 7
+		And reroll: 2 diamond, 1 coin
+		And stash: index 4
+		And reroll: 1 skull, 1 coin
+		And end turn;
+		Then score: 600
+		
